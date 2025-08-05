c9f295028b84e97de3c63fe63dff7743a22ec8d2
Vidit Saxena <viditsaxena2312@gmail.com>
2025-08-06 04:22:02 +0530

    feat: TopPanel implemented with static data

---
+++ portfolio-webapp/src/components/TopPanel.tsx
@@ -0,0 +1,19 @@
+import '../pages/Dashboard.css'
+
+function TopPanel() {
+  return (
+    <div className='top-panel'>
+        <div className="activityBar">
+            <b>Vidit's Portfolio</b>
+        </div>
+        <div className="dateTime">
+            Aug 6  12:40 AM
+        </div>
+        <div className="systemInfo">
+            System Information
+        </div>
+    </div>
+  )
+}
+
+export default TopPanel
