ba24b5c89014033aea5c02487203b7dc5068052e
Vidit Saxena <viditsaxena2312@gmail.com>
2025-08-06 04:06:08 +0530

    feat: implement the home screen of the OS

---
+++ portfolio-webapp/src/pages/DashBoard.tsx
@@ -0,0 +1,15 @@
+import Dock from '../components/Dock'
+import TopPanel from '../components/TopPanel'
+import './Dashboard.css'
+
+function DashBoard() {
+  return (
+    <div className='dashboard'>
+        <TopPanel />
+        <img className="background-image" src='/image-asset.jpeg'/>
+        <Dock />
+    </div>
+  )
+}
+
+export default DashBoard
